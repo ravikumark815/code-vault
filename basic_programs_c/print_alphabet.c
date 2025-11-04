d226a19daa8f0113aca04fceeee1da3fa27f5f92
Ravi Koothati <ravikumark815@gmail.com>
2025-11-04 15:28:50 -0500

    Alphabet loop

---
+++ basic_programs_c/print_alphabet.c
@@ -0,0 +1,26 @@
+/*
+Author: Ravi Koothati
+GitHub: https://github.com/ravikumark815
+
+This file is part of the 'code-vault' repository.
+Feel free to explore and contribute!
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+    printf("Using characters:");
+    for (int i = 'A'; i <= 'Z'; i++){
+        printf("%c", i);
+    }
+    
+    printf("\nUsing ASCII:");
+    for (int i = 65; i <= 90; i++){
+        printf("%c", i);
+    }
+    printf("\n");
+
+    return 0;
+}
