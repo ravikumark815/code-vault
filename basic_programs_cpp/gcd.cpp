12bd54d5af75b70d07f22a3f5617fea4275b83ce
Ravi Koothati <ravikumark815@gmail.com>
2025-11-11 19:22:27 -0500

    GCD

---
+++ basic_programs_cpp/gcd.cpp
@@ -0,0 +1,35 @@
+/*
+Author: Ravi Koothati
+GitHub: https://github.com/ravikumark815
+
+This file is part of the 'code-vault' repository.
+Feel free to explore and contribute!
+*/
+
+#include <iostream>
+using namespace std;
+
+int gcd (int a, int b) {
+    if (a == 0) return b;
+    if (b == 0) return a;
+    if (a == b) return a;
+
+    int min = b;
+    if (a < b) min = a;
+
+    for (int i = min; i >= 2; i--) {
+        if (((a % i) == 0) && ((b % i) == 0)) return i;
+    }
+    return 0;
+}
+
+int main()
+{
+    int a, b;
+    
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+
+    cout << "GCD: " << gcd(a, b) << endl;
+    return 0;
+}
