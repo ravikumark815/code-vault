525375a46507d90fcf660eab0a96a98b67d94cf0
Ravi Koothati <ravikumark815@gmail.com>
2025-11-15 17:10:44 -0500

    Hello World

---
+++ basic_programs_cpp/hello_world.cpp
@@ -0,0 +1,17 @@
+/*
+Author: Ravi Koothati
+GitHub: https://github.com/ravikumark815
+
+This file is part of the 'code-vault' repository.
+Feel free to explore and contribute!
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout << "Hello World!\n";
+    return 0;
+}
